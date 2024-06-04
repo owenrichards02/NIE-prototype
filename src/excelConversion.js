0909a287e1cba5470f2f1bbd2699f8bae631f6bc
Owen Richards <owen.p.richards@gmail.com>
2024-06-04 17:03:34 +0100

    working excel survey to HTML conversion (currently set up to only do the first good response! slice statement on line 36 needs to be removed)

--- src/excelConversion.js
+++ src/excelConversion.js
@@ -11,10 +11,65 @@ function excel_2_JSON(filepath){
     return jsonsheet
 }
 
+/* 
+Proposed HTML format for survey responses.
+
+<div data-surveyID=’x’>
+	<li data-responseID=’x’>
+		<div>
+	        <p class=”question”><b>questionText</b></p>
+			<p class=”answer”>answerText<\p>
+		</div>
+
+    </li>
+</div>
+
+*/
+function JSON_2_HTML(json, surveyID){
+
+    //might need to exclude tab and newline characters
+
+    //remove first row (contains full questions)
+    let htmlOut = '<div data-surveyID="' + surveyID + '">\n'
+    const titleRow = json.shift()
+
+    json = json.slice(1,2) //REMOVE AFTER TESTING!!
+    //to test on the first good row
+
+    for (const response of json){
+        htmlOut += ('<li data-responseID="' + response['ResponseId'] + '">\n')
+        for (const q_key in response){
+            let answer = response[q_key]
+            htmlOut += ('<div>\n')
+            htmlOut += ('\t<p class="question"><b>' + q_key + "</b></p>\n")
+            htmlOut += ('\t<p class="answer">' + answer + "</p>\n")
+            htmlOut += ('</div>\n')
+
+        }
+        htmlOut += ('</li>\n')
+    }
+
+    htmlOut += ('</div>\n')
+
+
+    return htmlOut
+}
+
+export function excelSurveyToHTML(filepath, surveyID){
+    const jsonsheet = excel_2_JSON(filepath)
+    //console.log(jsonsheet)
+    const htmlout = JSON_2_HTML(jsonsheet, surveyID)
+    return htmlout
+}
+
+
 
 function main(){
-    const jsonsheet = excel_2_JSON("./resources/survey_example.xlsx")
-    console.log(jsonsheet[2])
+    let surveyID = 75 //TEST -- MAKE SURE TO GET THIS PROPERLY
+    let filepath = "./resources/survey_example.xlsx"
+    const htmlout = excelSurveyToHTML(filepath, surveyID)
+
+    console.log(htmlout)
 }
 
 main()
