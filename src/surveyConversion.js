44fccb4701903745a872ed16e3f61e111c3aa887
Owen Richards <owen.p.richards@gmail.com>
2024-06-05 13:05:11 +0100

    added working transcript to HTML conversion

--- src/surveyConversion.js
+++ src/surveyConversion.js
@@ -1,6 +1,7 @@
 //import { readFile, utils } from "xlsx";
 
 import pkg from 'xlsx';
+import { writehtmlBacktoFile } from './fileTools.js';
 const { readFile, utils } = pkg;
 
 function excel_2_JSON(filepath){
@@ -33,20 +34,20 @@ function JSON_2_HTML(json, surveyID){
     let htmlOut = '<div data-surveyID="' + surveyID + '">\n'
     const titleRow = json.shift()
 
-    json = json.slice(1,2) //REMOVE AFTER TESTING!!
+    //json = json.slice(1,2) //REMOVE AFTER TESTING!!
     //to test on the first good row
 
     for (const response of json){
-        htmlOut += ('<li data-responseID="' + response['ResponseId'] + '">\n')
+        htmlOut += ('<ul data-responseID="' + response['ResponseId'] + '">\n')
         for (const q_key in response){
             let answer = response[q_key]
-            htmlOut += ('<div>\n')
+            htmlOut += ('<li class="qna">\n')
             htmlOut += ('\t<p class="question"><b>' + q_key + "</b></p>\n")
             htmlOut += ('\t<p class="answer">' + answer + "</p>\n")
-            htmlOut += ('</div>\n')
+            htmlOut += ('</li>\n')
 
         }
-        htmlOut += ('</li>\n')
+        htmlOut += ('</ul>\n')
     }
 
     htmlOut += ('</div>\n')
@@ -68,8 +69,9 @@ function main(){
     let surveyID = 75 //TEST -- MAKE SURE TO GET THIS PROPERLY
     let filepath = "./resources/survey_example.xlsx"
     const htmlout = excelSurveyToHTML(filepath, surveyID)
+    writehtmlBacktoFile(htmlout, "./resources/survey_out.html")
 
-    console.log(htmlout)
+    //console.log(htmlout)
 }
 
-//main()
+main()
